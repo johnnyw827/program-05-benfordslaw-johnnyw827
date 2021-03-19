cf703aa2036bebb924ec6b2f83f3d97993e2da7e
Johnny Williams <johnnyw827@gmail.com>
2021-03-19 16:04:12 -0400

    more

--- scrapNBA.py
+++ scrapNBA.py
@@ -1,18 +1,31 @@
 import pandas as pd
 from bs4 import BeautifulSoup
 import requests
+import re
+import pandas as pd
+def scrape():
+    nbawebsite = 'https://basketballnoise.com/nba-players-height-2019-2020/'
 
-nbawebsite = 'https://basketballnoise.com/nba-players-height-2019-2020/'
+    tes = []
+    data = requests.get(nbawebsite)
+    soup = BeautifulSoup(data.text,'html.parser')
 
-height_list = []
 
-data = requests.get(nbawebsite)
-soup = BeautifulSoup(data.text,'html.parser')
+    add_period = ['’']
 
-#height_tables = soup.findAll('td')
-#print(height_tables[14].get_text())
+    remove = [',',"\\","'",'"','[',']','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',"-"]
+    for height in soup.find_all(class_='wp-block-table is-style-stripes'):
+        tes.append(height.tr.td.text)
+        table1 = str(tes[0:])
 
-tables = soup.find(class_='wp-block-table is-style-stripes')
-for tr in tables.findAll('tr'):
-    place = tr.findAll('td')[1].text
-    print(place[0])
+    for i in remove:
+        table1 = table1.replace(i,'')
+    for i in add_period:
+        table1 = table1.replace(i,'.')
+
+    ls = table1.split()
+    ls2 = [s.strip('.')for s in ls]
+
+    with open('./nbafile.txt','w') as writing:
+        for i in ls2:
+            writing.writelines('%s\n'%i)
